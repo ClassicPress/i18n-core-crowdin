176904200e8e5054bfdcc10326fcfe7bd36ba87d
mattyrob <mattyrobuk@gmail.com>
2025-02-02 16:16:58 +0000

    Feat: Create and add JSON files to translation packages

--- bin/build_zip.sh
+++ bin/build_zip.sh
@@ -27,13 +27,7 @@ mkdir -p zips
 dir="./translations"
 
 cd "$dir"
-
-# clear any existing *.mo files
-echo 'Removing existing MO files for locale'
-mofiles=""
-for f in *${locales}.mo; do
-	rm "${f}" || true
-done
+mkdir "$locales"
 
 # create a list of all available locales
 echo 'Creating list of PO files for locale'
@@ -42,33 +36,63 @@ for f in *${locales}.po; do
 	pofiles+="${f} "
 done
 
-# make *.mo files for all *.po files
-echo ''
-echo 'Creating mo files'
+echo 'Copying PO files to working locale folder'
 for n in ${pofiles[@]};
 do
-	wp i18n make-mo "${n}"
+	cp "${n}" "$locales"
 done
 
-pomofiles=""
-for f in *${locales}.{po,mo}; do
-	pomofiles+="${f} "
-done
+pushd "$locales" > /dev/null
+	# make *.mo files for all *.po files
+	echo 'Working in locale folder'
+	echo ''
+	echo 'Creating mo files'
+	for n in ${pofiles[@]};
+	do
+		wp i18n make-mo "${n}"
+	done
 
-echo ''
-echo 'Creating zip file'
-for n in ${pomofiles[@]};
-do
-	zip "../zips/$locales.zip" "$n"
-done
+	echo ''
+	echo 'Creating JSON files'
+	wp i18n make-json --no-purge .
+	
+	pomofiles=""
+	for f in *${locales}.{po,mo}; do
+		pomofiles+="${f} "
+	done
+	
+	echo ''
+	echo 'Creating zip file'
+	for n in ${pomofiles[@]};
+	do
+		zip "../../zips/$locales.zip" "$n"
+	done
+
+	for f in admin-*${locales}*.json; do
+		mv "${f}" "${f#admin-}"
+	done
+	
+	jsonfiles=""
+	for f in *${locales}*.json; do
+		jsonfiles+="${f} "
+	done
+
+	for n in ${jsonfiles[@]};
+	do
+		zip "../../zips/$locales.zip" "$n"
+	done
+popd > /dev/null
+
+echo 'Removing working locale folder'
+rm -rf "$locales"
 
 # report zip file name and creation date/time
 echo ''
 echo "$locales.zip created"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	stat -c '%.19z' en_GB.zip
+	stat -c '%.19z' "../../zips/$locales.zip"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	stat -f %SB -t '%Y-%m-%d %H:%M:%S' "../zips/$locales.zip"
+	stat -f %SB -t '%Y-%m-%d %H:%M:%S' "../../zips/$locales.zip"
 fi
 
 # human readable to minified
